1826ce82f8cafa9a2894d0bae108d4c9bcf5e146
VictorLay <Viktor.latyshey@mail.ru>
2022-02-26 13:59:43 +0300

    Added command which return String error message description.

--- src/main/java/by/epam/latyshey/library/view/menu/menuImpl/ErrorViewImpl.java
+++ src/main/java/by/epam/latyshey/library/view/menu/menuImpl/ErrorViewImpl.java
@@ -6,6 +6,12 @@ public class ErrorViewImpl implements MenuCreate {
 
   @Override
   public void executeResponse(String response) {
-    System.out.println(response);
+
+    System.out.println(
+        "================================================================\n\n" +
+        "В программе возникло несоответствие:\n" +
+        response.substring(response.indexOf(",")+1) + "\n" +
+        "================================================================\n\n"
+    );
   }
 }
